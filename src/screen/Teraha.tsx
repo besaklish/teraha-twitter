eff3a1745f612cb6335dee28c362192db6521a0a
besaklish <besaklish@gmail.com>
2021-03-26 23:42:55 +0900

    Change form style in Teraha

--- src/screen/Teraha.tsx
+++ src/screen/Teraha.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import TerahaInfo from "./Teraha.json";
 
 // interfaces
-interface terahaState{
-  series: number,
-  part: number,
-  episode: number,
-  searchWord: string,
-};
+interface terahaState {
+  series: number;
+  part: number;
+  episode: number;
+  searchWord: string;
+}
 
 // main component
-class Teraha extends React.Component<{}, terahaState>{
-  constructor(props: {}){
+class Teraha extends React.Component<{}, terahaState> {
+  constructor(props: {}) {
     super(props);
     this.state = {
       series: 1,
       part: 1,
       episode: 1,
-      searchWord: "テラハ"
+      searchWord: "テラハ",
     };
 
     this.generateTwitterLink = this.generateTwitterLink.bind(this);
@@ -29,16 +29,25 @@ class Teraha extends React.Component<{}, terahaState>{
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  addDays(date: string, days: number): Date{
+  addDays(date: string, days: number): Date {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
   }
 
-  generateTwitterLink(series: number, part: number, episode: number, searchWord: string, untilDays: number = 3): string {
-    try{
-      console.log(`generateTwitterLink, series ${series}, part ${part}, episode ${episode}`);
-      const episodeInfo = TerahaInfo[series - 1].parts[part - 1].episodes[episode - 1];
+  generateTwitterLink(
+    series: number,
+    part: number,
+    episode: number,
+    searchWord: string,
+    untilDays: number = 3
+  ): string {
+    try {
+      console.log(
+        `generateTwitterLink, series ${series}, part ${part}, episode ${episode}`
+      );
+      const episodeInfo =
+        TerahaInfo[series - 1].parts[part - 1].episodes[episode - 1];
       if (episodeInfo) {
         const since = episodeInfo.date;
         const untilDate = this.addDays(since, untilDays);
@@ -46,68 +55,85 @@ class Teraha extends React.Component<{}, terahaState>{
         return `https://twitter.com/search?q=${searchWord}%20until%3A${until}%20since%3A${since}`;
       }
       return "https://twitter.com/home";
-    } catch(e){
+    } catch (e) {
       console.error(e);
       return "https://twitter.com/home";
     }
   }
 
-  handleSeriesChange(e: React.ChangeEvent<HTMLSelectElement>){
+  handleSeriesChange(e: React.ChangeEvent<HTMLSelectElement>) {
     const tsCopy = JSON.parse(JSON.stringify(this.state));
     tsCopy.series = parseInt(e.target.value);
     this.setState(tsCopy);
   }
 
-  handlePartChange(e: React.ChangeEvent<HTMLSelectElement>){
+  handlePartChange(e: React.ChangeEvent<HTMLSelectElement>) {
     const tsCopy = JSON.parse(JSON.stringify(this.state));
     tsCopy.part = parseInt(e.target.value);
     this.setState(tsCopy);
   }
 
-  handleEpisodeChange(e: React.ChangeEvent<HTMLSelectElement>){
+  handleEpisodeChange(e: React.ChangeEvent<HTMLSelectElement>) {
     const tsCopy = JSON.parse(JSON.stringify(this.state));
     tsCopy.episode = parseInt(e.target.value);
     this.setState(tsCopy);
   }
 
-  handleSearchWordChange(e: React.ChangeEvent<HTMLInputElement>){
+  handleSearchWordChange(e: React.ChangeEvent<HTMLInputElement>) {
     const tsCopy = JSON.parse(JSON.stringify(this.state));
     tsCopy.searchWord = e.target.value;
     this.setState(tsCopy);
   }
 
-  handleSubmit(e: React.FormEvent<HTMLFormElement>){
+  handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const series = parseInt(formData.get("series") as string);
     const part = parseInt(formData.get("part") as string);
     const episode = parseInt(formData.get("episode") as string);
     const searchWord = formData.get("searchWord") as string;
-    if (series && part && episode){
+    if (series && part && episode) {
       this.setState({
         series: series,
         part: part,
-        episode: episode
-      })
+        episode: episode,
+      });
     }
-    console.log(`series ${formData.get("series")}, part ${formData.get("part")}, episode ${formData.get("episode")}`)
-    const twitterLink = this.generateTwitterLink(series, part, episode, searchWord);
+    console.log(
+      `series ${formData.get("series")}, part ${formData.get(
+        "part"
+      )}, episode ${formData.get("episode")}`
+    );
+    const twitterLink = this.generateTwitterLink(
+      series,
+      part,
+      episode,
+      searchWord
+    );
     window.open(twitterLink, "_blank", "noopener,noreferrer");
     console.log(e);
   }
 
-  render(){
+  render() {
     const jsxParts: JSX.Element[] = [];
     const seriesInfo = TerahaInfo[this.state.series - 1];
     for (let i = 1; i <= seriesInfo.parts.length; i++) {
-      jsxParts.push(<option value={i} key={i}>Part {i}</option>)
+      jsxParts.push(
+        <option value={i} key={i}>
+          Part {i}
+        </option>
+      );
     }
-  
+
     const jsxEpisodes: JSX.Element[] = [];
     const partInfo = seriesInfo.parts[this.state.part - 1];
-    if (partInfo){
+    if (partInfo) {
       for (let i = 1; i <= partInfo.episodes.length; i++) {
-        jsxEpisodes.push(<option value={i} key={i}>Episode {i}: {partInfo.episodes[i - 1].title}</option>)
+        jsxEpisodes.push(
+          <option value={i} key={i}>
+            Episode {i}: {partInfo.episodes[i - 1].title}
+          </option>
+        );
       }
     }
 
@@ -115,29 +141,61 @@ class Teraha extends React.Component<{}, terahaState>{
       <div>
         <h1>テラハ放送直後のTwitter</h1>
         <form onSubmit={this.handleSubmit}>
-          {/* Choose Series */}
-          <select name="series" id="series" defaultValue={this.state.series} onChange={this.handleSeriesChange}>
-            <option value="1">BOYS×GIRLS NEXT DOOR (2012–2014, 湘南)</option>
-            <option value="2">BOYS & GIRLS IN THE CITY (2015-2016, 東京)</option>
-            <option value="3">ALOHA STATE (2016-2017, ハワイ)</option>
-            <option value="4">OPENING NEW DOORS (2017-2018, 軽井沢)</option>
-            <option value="5">TOKYO (2019-2020, 東京)</option>
-          </select>
-          {/* Choose Part */}
-          <select name="part" id="part" defaultValue={this.state.part} onChange={this.handlePartChange}>
-            {jsxParts}
-          </select>
-          {/* Choose Episode */}
-          <select name="episode" id="episode" defaultValue={this.state.episode} onChange={this.handleEpisodeChange}>
-            {jsxEpisodes}
-          </select>
-          <input type="text" name="searchWord" id="searchWord" defaultValue={this.state.searchWord} onChange={this.handleSearchWordChange}/>
-          <button type="submit">Twitterへのリンク</button>
+          <div>
+            {/* Choose Series */}
+            <select
+              name="series"
+              id="series"
+              defaultValue={this.state.series}
+              onChange={this.handleSeriesChange}
+            >
+              <option value="1">BOYS×GIRLS NEXT DOOR (2012–2014, 湘南)</option>
+              <option value="2">
+                BOYS & GIRLS IN THE CITY (2015-2016, 東京)
+              </option>
+              <option value="3">ALOHA STATE (2016-2017, ハワイ)</option>
+              <option value="4">OPENING NEW DOORS (2017-2018, 軽井沢)</option>
+              <option value="5">TOKYO (2019-2020, 東京)</option>
+            </select>
+          </div>
+          <div>
+            {/* Choose Part */}
+            <select
+              name="part"
+              id="part"
+              defaultValue={this.state.part}
+              onChange={this.handlePartChange}
+            >
+              {jsxParts}
+            </select>
+          </div>
+          <div>
+            {/* Choose Episode */}
+            <select
+              name="episode"
+              id="episode"
+              defaultValue={this.state.episode}
+              onChange={this.handleEpisodeChange}
+            >
+              {jsxEpisodes}
+            </select>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="searchWord"
+              id="searchWord"
+              defaultValue={this.state.searchWord}
+              onChange={this.handleSearchWordChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Twitterへのリンク</button>
+          </div>
         </form>
       </div>
-    )
+    );
   }
-
 }
 
 export default Teraha;
